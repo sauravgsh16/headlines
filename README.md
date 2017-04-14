e65cde531d032506f14ed15a903582e1a9e75883
Saurav Ghosh <sauravgsh16@gmail.com>
2017-04-14 14:43:50 +0530

    rss feed

---
+++ README.md
@@ -0,0 +1,5 @@
+# headlines
+
+# RSS feed
+# We use a python library to abstarct away unwanted things which allows us to access data in a completely consistent fashion
+pip install feedparser
--- headlines.py
+++ headlines.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import feedparser
+
+BBC_feed = 'http://feeds.bbci.co.uk/news/rss.xml'
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_news():
-	return "these is no news"
+	feed = feedparser.parse(BBC_feed)
+	first_article = feed['enteries'][0]
+	return """<html>
+		<body>
+			<h1>BBC Headlines </h1>
+			<b>{0}</b><br/>
+			<b>{1}</b><br/>
+			<b>{2}</b><br/>
+		</body>
+	</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 if __name__ == "__main__":
 	app.run()
