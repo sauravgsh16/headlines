04a17ad449a69d2f1689d96f912c866ec9a1c346
Saurav Ghosh <sauravgsh16@gmail.com>
2017-04-14 16:16:58 +0530

    templates

--- headlines.py
+++ headlines.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 import feedparser
 
-BBC_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 			'cnn': 'http://rss.cnn.com/rss/edition.rss',
 			'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
@@ -23,16 +23,9 @@ def fox():
 
 def get_news(publication=None):
 	print publication
-	feed = feedparser.parse(BBC_FEED[publication])
+	feed = feedparser.parse(RSS_FEED[publication])
 	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1>BBC Headlines </h1>
-			<b>{0}</b><br/>
-			<b>{1}</b><br/>
-			<b>{2}</b><br/>
-		</body>
-	</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+	return render_template('home.html', title = first_article.get("title"), published = first_article.get("published"), summary = first_article.get("summary"))
 
 if __name__ == "__main__":
 	app.run()
